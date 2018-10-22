181c83d8d9d936cc07a4eb3813cd90872e44045f
unknown <zeke.buzzeo@gmail.com>
2018-10-21 17:14:33 -1000

    Exercise 1

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
+
+function spacer(exNum) {
+      console.log(`\nExercise: ${exNum}`);
+}
+
 //1. Makin Bacon
 /*Create a function named `moreContent` that will initiate the button after clicking on it.
 
 The function will add the following content inside the paragraph with the id of `more`.
 */
 
+let pElem = document.getElementById('more');
+
 var bacon = "Shoulder turducken brisket, kevin swine andouille tri-tip salami tail ham sausage pork loin. Ribeye short loin rump kielbasa pork. Capicola short loin turducken corned beef tongue, chuck leberkas salami frankfurter. Kielbasa fatback pancetta, ground round meatball turducken jowl ribeye alcatra sirloin bacon corned beef beef ribs short loin. Pork belly spare ribs biltong corned beef meatball short ribs tongue alcatra swine drumstick. Biltong shankle kevin, cupim sirloin bresaola brisket. Tail pork belly biltong ball tip tri-tip, pig jerky cow pastrami prosciutto ;ground round bacon capicola tongue meatball.";
 
+function moreContent() {
+      pElem.innerHTML = bacon;
+
+}
 
 //2. HTTP
 /*Create a function named `lessContent` that will initiate the `Show Less` link after clicking on it.
